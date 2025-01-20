149681685f4fec791eb3f3fba51ff974a7906f7e
Iker Hurtado Caballo <ikerhurcab@campus.monlau.com>
2025-01-20 18:37:05 +0100

    done testing

--- dashboard.js
+++ dashboard.js
@@ -130,6 +130,18 @@ darkModeToggle.addEventListener('change', () => {
             <path d="M12 2.25a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75ZM7.5 12a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM18.894 6.166a.75.75 0 0 0-1.06-1.06l-1.591 1.59a.75.75 0 1 0 1.06 1.061l1.591-1.59ZM21.75 12a.75.75 0 0 1-.75.75h-2.25a.75.75 0 0 1 0-1.5H21a.75.75 0 0 1 .75.75ZM17.834 18.894a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 1 0-1.061 1.06l1.59 1.591ZM12 18a.75.75 0 0 1 .75.75V21a.75.75 0 0 1-1.5 0v-2.25A.75.75 0 0 1 12 18ZM7.758 17.303a.75.75 0 0 0-1.061-1.06l-1.591 1.59a.75.75 0 0 0 1.06 1.061l1.591-1.59ZM6 12a.75.75 0 0 1-.75.75H3a.75.75 0 0 1 0-1.5h2.25A.75.75 0 0 1 6 12ZM6.697 7.757a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 0 0-1.061 1.06l1.59 1.591Z" />
         `;
     }
+
+    const labels = content.querySelectorAll('label');
+    labels.forEach(label => {
+        label.style.color = darkModeToggle.checked ? '#f7fafc' : '#1a202c';
+    });
+
+    const inputs = content.querySelectorAll('input');
+    inputs.forEach(input => {
+        input.style.backgroundColor = darkModeToggle.checked ? '#2d3748' : '#e2e8f0';
+        input.style.color = darkModeToggle.checked ? '#f7fafc' : '#1a202c';
+    });
+    
 });
 
 document.getElementById('hamburger').addEventListener('click', () => {
